eddad4bb8814227df7b2471a5022e6217ce9e6b8
bling-two-one <artwins007@gmail.com>
2023-11-06 18:52:44 +0900

    cimmit

--- farok/farok_render.py
+++ farok/farok_render.py
@@ -2,24 +2,34 @@ import pygame
 
 pygame.init()
 
+#색
+bg = [38, 50, 54]
+
+#이미지
+title_img = pygame.image.load("Image\\Title\\title.png")
+
+#게임 화면
+pygame.display.set_caption("Farok_Engine")
 size = [1000, 500]
 screen = pygame.display.set_mode(size)
 
-pygame.display.set_caption("Farok_Engine")
-
-close = False
+#게임 종료 판정
+run = True
 clock = pygame.time.Clock()
 
-while not close:
-  
-    # This limits the while loop to a max of 10 times per second.
-    # Leave this out and we will use all CPU we can.
+while(True):
+
+    #프레임 제한(현재 30)
     clock.tick(30)
-     
-    # Main Event Loop
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            close=True # Flag that we are done so we exit this loop
+
+    #타이틀 및 기본 화면 설정
+    screen.fill(bg)
+    screen.blit(title_img, (size[0]/3, size[1]/3))
+
+    #이벤트 감지
+    for event in pygame.event.get(): #유저가 뭔가 함
+        if event.type == pygame.QUIT: #유저가 누름 == 무엇
+            close=False #무엇 함
 
     pygame.display.flip()
     
